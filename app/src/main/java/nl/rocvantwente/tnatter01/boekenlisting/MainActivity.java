5146e36437e641bfd346f1c4b5afaedd4bcab962
Twan Natter <twan180401@gmail.com>
2019-04-04 12:18:02 +0200

    Initial commit

---
+++ app/src/main/java/nl/rocvantwente/tnatter01/boekenlisting/MainActivity.java
@@ -0,0 +1,13 @@
+package nl.rocvantwente.tnatter01.boekenlisting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
